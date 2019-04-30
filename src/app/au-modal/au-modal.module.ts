f9f12d880f177f8b1a065f4097f974a20634e20b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-30 07:52:53 -0700

    starter files

---
+++ src/app/au-modal/au-modal.module.ts
@@ -0,0 +1,30 @@
+
+
+
+
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class AuModalModule {
+
+
+
+
+
+
+
+
+
+}
+
+
+
